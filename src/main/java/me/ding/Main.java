4e53bb0a781448a2e35faead94e216cce00b2c84
ding <69354087+prgding@users.noreply.github.com>
2023-04-03 23:58:39 +0800

    new words

--- src/main/java/me/ding/Main.java
+++ src/main/java/me/ding/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 		// 读取txt文件内容并存入Map
 		try {
-			File file = new File("input.txt"); // 文件路径
+			File file = new File("input-front50.txt"); // 文件路径
 			Scanner scanner = new Scanner(file);
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
@@ -31,6 +31,7 @@ public class Main {
 		}
 
 		int count = 1;
+		String strange = "";
 
 		// 不断循环，直到用户输入"exit"为止
 		while (true) {
@@ -39,7 +40,7 @@ public class Main {
 			// 创建一个Scanner对象，读取用户输入
 			Scanner scanner = new Scanner(System.in);
 			// 生成一个随机数，获取对应的key和value
-			String key = getRandomUnaskedKey(myMap, askedMap, random);
+			String key = getRandomUnaskedKey(myMap, askedMap, random, strange);
 			String value = myMap.get(key);
 			askedMap.put(key, true);
 
@@ -54,10 +55,22 @@ public class Main {
 				scanner.close();
 				break;
 			}
+			if (input.equals("1")) {
+				// 加入不熟悉队列
+				strange = mark(strange, key, value);
+				continue;
+			}
+			if (input.equals("strange")) {
+				System.out.println(strange);
+			}
 
 			while (!input.equals(value)) {
 				System.out.println("不对，请重新输入: " + key + " [" + count + "]");
+				strange = mark(strange, key, value);
 				input = scanner.nextLine();
+				if (input.equals("v")) {
+					System.out.println(value);
+				}
 			}
 
 			System.out.println("对了!");
@@ -65,18 +78,29 @@ public class Main {
 		}
 	}
 
+	private static String mark(String strange, String key, String value) {
+		String single = "陌生的词组: " + key + ":" + value;
+		strange += single + '\n';
+		return strange;
+	}
+
 	// 获取一个尚未被提问过的key
-	private static String getRandomUnaskedKey(Map<String, String> myMap, Map<String, Boolean> askedMap, Random random) {
+	private static String getRandomUnaskedKey(Map<String, String> myMap, Map<String, Boolean> askedMap, Random random
+			, String strange) {
 		String[] unaskedKeys = askedMap.keySet().stream().filter(key -> !askedMap.get(key)).toArray(String[]::new);
 		if (unaskedKeys.length > 0) {
 			return unaskedKeys[random.nextInt(unaskedKeys.length)];
 		} else {
 			// 如果所有的key都已经被提问过，则重新设置所有key的标记，并返回一个随机的key
-			System.out.println("全部提问过了，重新开始");
-			for (String key : myMap.keySet()) {
-				askedMap.put(key, false);
-			}
-			return myMap.keySet().toArray(new String[0])[random.nextInt(myMap.size())];
+			System.out.println("全部提问过了");
+			System.out.println(strange);
+			System.exit(0);
+
+//			for (String key : myMap.keySet()) {
+//				askedMap.put(key, false);
+//			}
+//			return myMap.keySet().toArray(new String[0])[random.nextInt(myMap.size())];
 		}
+		return null;
 	}
 }
--- src/main/java/me/ding/test.java
+++ src/main/java/me/ding/test.java
@@ -1,32 +1,22 @@
 package me.ding;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.*;
 
 public class test {
 	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<>();
-		map.put("扒出", "rake out");
-		map.put("燃烧着的煤", "live coals");
-		map.put("公鸡在打鸣", "cocks are crowing");
+		String fileName = "input.txt";
+		int linesToRead = 5;
 
-		Scanner scanner = new Scanner(System.in);
-
-		for (String key : map.keySet()) {
-			System.out.println("请回答以下问题的value: " + key);
-			String userAnswer = scanner.nextLine();
-
-			while (!userAnswer.equals(map.get(key))) {
-				System.out.println("答案不正确，请重新输入: " + key);
-				userAnswer = scanner.nextLine();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			int lineNumber = 0;
+			while ((line = br.readLine()) != null && lineNumber < linesToRead) {
+				System.out.println(line);
+				lineNumber++;
 			}
-
-			System.out.println("回答正确，下一个问题。");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
-		System.out.println("恭喜你，回答了所有问题！");
-		scanner.close();
 	}
 }
 
